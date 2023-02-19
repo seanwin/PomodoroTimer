d448ba0d4c9edbac9447b51555a948e9e7d0e260
An Nguyen <swagsome@gmail.com>
2023-02-19 13:53:51 +0900

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,20 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by An Nguyen on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
